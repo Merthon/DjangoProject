7dec8237977f5f484a5b9a6ecf9c53da09e2dbb7
Merthon <imchenxing42@gmail>
2025-03-24 01:47:04 +0800

    fix: delete test

--- HelloWorld/HelloWorld/urls.py
+++
@@ -1,8 +0,0 @@
-# 该 Django 项目的 URL 声明; 一份由 Django 驱动的网站"目录"。
-from django.urls import path
- 
-from . import views
- 
-urlpatterns = [
-    path("", views.hello, name="hello"),
-]
--- mall_project/payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'payments'
-urlpatterns = [
-    path('pay/<int:order_id>/', views.process_payment, name='process_payment'),  # 处理支付
-    path('callback/', views.payment_callback, name='payment_callback'),          # 支付回调
-]
